610b5cfc69850c28d8e013565b40c010921368e3
Yanie <maanadelacruz@gbox.ncf.edu.ph>
2025-05-24 06:33:34 +0800

    Latest Update

---
+++ src/assets/js/Data.js
@@ -0,0 +1,25 @@
+
+
+
+// export default  {
+//   // API URLs
+//   API_URL: import.meta.env.VITE_API_URL,
+//   SOCKET_URL: import.meta.env.VITE_SOCKET_URL,
+//   SOCKET_URL2: import.meta.env.VITE_SOCKET_URL2,
+
+//   // API Endpoints
+//   LOGIN: "/auth/login",
+//   LOGOUT: "/auth/logout",
+//   FORGOT_PASSWORD: "/auth/forgot-password",
+//   RESET_PASSWORD: "/auth/reset-password",
+//   GET_USER_PROFILE: "/user/profile",
+//   UPDATE_USER_PROFILE: "/user/update-profile",
+//   GET_USER_SETTINGS: "/user/settings",
+//   UPDATE_USER_SETTINGS: "/user/update-settings",
+
+//   // Socket Events
+//   SOCKET_CONNECT: "connect",
+//   SOCKET_DISCONNECT: "disconnect",
+
+
+// };
